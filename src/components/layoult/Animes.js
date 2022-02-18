bdcc08ff3387f3248e731ca9b45c50cf0851c3e5
LukeRekt <lucasrsl12@gmail.com>
2022-02-18 20:24:11 -0300

    carousel praticamente terminado

--- src/components/layoult/Animes.js
+++ src/components/layoult/Animes.js
@@ -5,9 +5,11 @@ function Anime(props) {
             <div className={styles.anime_box}>
             <img src={props.imagem} alt="" />
             <div className={styles.anime_over}>       
+                <div className={styles.anime_infos}>
                 <p>Arifureta Shokugyou...</p>      
-                <p>ANIME</p> 
-                <p>6 EPISÓDEOS</p>         
+                <div className={styles.anime_botao}>ANIME</div> 
+                <p>6 EPISÓDEOS</p>  
+                </div>       
             </div>
             </div>
         </div>
