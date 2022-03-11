212f44215658313b02396390ec35fb09a3bacfa2
LukeRekt <lucasrsl12@gmail.com>
2022-03-11 16:53:08 -0300

    form de login integrado com backend

--- src/components/pages/PageLogin.js
+++ src/components/pages/PageLogin.js
@@ -3,12 +3,36 @@ import { useState } from "react"
 import styles from "./PageLogin.module.css"
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+
+import {login} from '../../api/user'
+
 
 function PageLogin(){
+
+    const navigate = useNavigate();
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await login({email, password});
+            if(res.error) toast.error(res.error);
+            else{
+                toast.success(res.message);
+
+                navigate("/")
+            }
+        }catch (err){
+            toast.error(err);
+        }
+    }
 
     return (
         <div className={styles.container}>
@@ -42,7 +66,8 @@ function PageLogin(){
                 </FormControl>
             </div>
             <div className="textCenter">
-                <Button variant="contained" disabled={!email || !password}>Enviar</Button>
+                <Button variant="contained" disabled={!email || !password}
+                onClick={handleLogin}>Enviar</Button>
             </div>
         </div>
     )
