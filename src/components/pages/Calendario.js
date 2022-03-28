399b6f68a89bcdd06ba853d265ebfacbf5e41336
LukeRekt <lucasrsl12@gmail.com>
2022-03-28 18:20:31 -0300

    tamanho padrao do calendário

--- src/components/pages/Calendario.js
+++ src/components/pages/Calendario.js
@@ -1,8 +1,10 @@
-
+import styles from "./Calendario.module.css"
 function Calendario() {
     return (//Deve ser possível adicionar no calendário por contas autorizadas  
-        <div>
-            <h1>Calendario</h1>
+        <div className={styles.container}>
+            <div className={styles.insideContainer}>
+               
+            </div>
         </div>
     )
 }//Esta página tem prioridade no desenvolvilmeto
