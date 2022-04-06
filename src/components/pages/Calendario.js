78f506976468efc64620bc7ed27c38ca30d829e6
LukeRekt <lucasrsl12@gmail.com>
2022-04-06 11:39:32 -0300

    estilo calendário

--- src/components/pages/Calendario.js
+++ src/components/pages/Calendario.js
@@ -11,41 +11,53 @@ function Calendario() {
     return (//Deve ser possível adicionar no calendário por contas autorizadas  
         <div className={styles.container}>
             <div className={styles.BtnSemana}>
-                <div onClick={() => toggleTab(1)} className={`${styles.favoritos} ${styles.tab_color}`} ><p> Favoritos</p></div>
-                <div onClick={() => toggleTab(2)} className={`${styles.assistidos} ${styles.tab_color}`}><p> Assistidos</p></div>
-                <div onClick={() => toggleTab(3)} className={`${styles.maistarde} ${styles.tab_color}`}><p>Ver mais Tarde</p></div>
-                <div onClick={() => toggleTab(5)} className={`${styles.editarPerfil} ${styles.tab_color}`}><p> Editar Perfil</p></div>
-                <div className={`${styles.algumacoisa} ${styles.tab_color}`}><p>Logout</p></div>
-
+                <div onClick={() => toggleTab(1)} className={toggleState === 1 ? `${styles.favoritos}  ${styles.tab_color} ${styles.tab_color_active}` : `${styles.favoritos}  ${styles.tab_color} `} ><p>Domingo</p></div>
+                <div onClick={() => toggleTab(2)} className={toggleState === 2 ? `${styles.assistidos}  ${styles.tab_color} ${styles.tab_color_active}` : `${styles.favoritos}  ${styles.tab_color} `}><p>Segunda</p></div>
+                <div onClick={() => toggleTab(3)} className={toggleState === 3 ? `${styles.maistarde}  ${styles.tab_color} ${styles.tab_color_active}` : `${styles.favoritos}  ${styles.tab_color} `}><p>Terça</p></div>
+                <div onClick={() => toggleTab(4)} className={toggleState === 4 ? `${styles.editarPerfil}  ${styles.tab_color} ${styles.tab_color_active}` : `${styles.favoritos}  ${styles.tab_color} `}><p>Quarta</p></div>
+                <div onClick={() => toggleTab(5)} className={toggleState === 5 ? `${styles.algumacoisa}  ${styles.tab_color} ${styles.tab_color_active}` : `${styles.favoritos}  ${styles.tab_color} `}><p>Quinta</p></div>
+                <div onClick={() => toggleTab(6)} className={toggleState === 6 ? `${styles.algumacoisa}  ${styles.tab_color} ${styles.tab_color_active}` : `${styles.favoritos}  ${styles.tab_color} `}><p>Sexta</p></div>
+                <div onClick={() => toggleTab(7)} className={toggleState === 7 ? `${styles.algumacoisa} ${styles.ultimo} ${styles.tab_color} ${styles.tab_color_active}` : `${styles.favoritos}  ${styles.tab_color} `}><p>Sábado</p></div>
             </div>
             <div className={styles.content_tabs}>
                 <div className={toggleState === 1 ? `${styles.content}  ${styles.active_content}` : `${styles.content}`}>
                     <div className={styles.naoTem}>
-                        <p> Você não tem nenhum favorito</p>
+                        <p> Nenhum episódio hoje</p>
                     </div>
 
                 </div>
 
                 <div className={toggleState === 2 ? `${styles.content}  ${styles.active_content}` : `${styles.content}`}>
                     <div className={styles.naoTem}>
-                        <p> Você não assistiu nada</p>
+                    <p> Nenhum episódio hoje</p>
                     </div>
                 </div>
 
                 <div className={toggleState === 3 ? `${styles.content}  ${styles.active_content}` : `${styles.content}`}>
                     <div className={styles.naoTem}>
-                        <p> Lista vazia</p>
+                    <p> Nenhum episódio hoje</p>
                     </div>
                 </div>
                 <div className={toggleState === 4 ? `${styles.content}  ${styles.active_content}` : `${styles.content}`}>
                     <div className={styles.naoTem}>
-                        <p>Você não tem nenhum favorito</p>
+                    <p> Nenhum episódio hoje</p>
                     </div>
                 </div>
                 <div className={toggleState === 5 ? `${styles.content}  ${styles.active_content}` : `${styles.content}`}>
-                    
+                <p> Nenhum episódio hoje</p>
+                </div>
+                <div className={toggleState === 6 ? `${styles.content}  ${styles.active_content}` : `${styles.content}`}>
+                    <div className={styles.naoTem}>
+                    <p> Nenhum episódio hoje</p>
+                    </div>
+                </div>
+                <div className={toggleState === 7 ? `${styles.content}  ${styles.active_content}` : `${styles.content} `}>
+                    <div className={styles.naoTem}>
+                    <p> Nenhum episódio hoje</p>
+                    </div>
                 </div>
             </div>
+            
         </div>
     )
 }//Esta página tem prioridade no desenvolvilmeto
