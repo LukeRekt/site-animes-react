519d98b6085f0b9c8c30b5ebdd815e92a4cfd32f
LukeRekt <lucasrsl12@gmail.com>
2022-04-07 14:39:21 -0300

    footer concluido

--- src/components/layoult/Footer.js
+++ src/components/layoult/Footer.js
@@ -10,9 +10,9 @@ function Footer() {
         <div className={styles.footer_navegacao}>
         <h3>Navegação</h3>
         <a href="/">Home</a>
-        <a href="/">Lista</a>
-        <a href="/">Calendário</a>
-        <a href="/">Github</a>
+        <a href="/lista">Lista</a>
+        <a href="/calendario">Calendário</a>
+        <a href="https://github.com/LukeRekt/site-animes-react">Github</a>
         </div>
         <div className={styles.footer_parceiros}>
         <h3>Os de vdd</h3>
@@ -22,13 +22,15 @@ function Footer() {
             <div className={styles.footer_redes}>
                 <h3>Redes Sociais</h3>
                 <a href=""><FaFacebookF />Facebook</a>
-                <a href=""><FaTwitter />Twitter</a>
                 <a href="https://discord.gg/6a4JT3fvrz" target="_blank"><FaDiscord />Discord</a>
             </div>
             <div className={styles.footer_disclamer}>
             <h3>Disclaimer</h3>
             <small>Desenvolvido para testes e nao uso comercial</small>
              </div>
+             </div>
+             <div className={styles.footer_small}> 
+             <small>By: LukeRekt with ❤</small>
              </div>
         </div>
     )
