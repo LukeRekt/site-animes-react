3baaff9a0426be57a39958fb7364a1e6cf9d41a3
LukeRekt <lucasrsl12@gmail.com>
2022-04-12 10:13:37 -0300

    primeiro teste barra progresso

--- src/components/layoult/EpisodiosHome/EpisodiosHome.js
+++ src/components/layoult/EpisodiosHome/EpisodiosHome.js
@@ -1,5 +1,6 @@
 import { AiFillPlayCircle } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import VideoProgress from "../VideoProgress/VideoProgress";
 
 import styles from "./EpisodiosHome.module.css"
 
@@ -11,25 +12,30 @@ function EpisodiosHome(props) {
                 <div className={styles.episodeInfos}>
 
                     <div className={styles.fundoAnime}>
+                    
                         <div className={styles.qualidade}>
                             <p>FHD</p>
+                            
                         </div>
-
+                        <div className={styles.progressoAnime}>
+                        <VideoProgress porcentagem="10"/>
+                        </div>
                         <div className={styles.textosAnime}>
                             <p>Episódio: {props.numero}</p>
                             <p>{props.nome}
                             </p>
                             <p>{props.anime}</p>
                         </div>
-
+                        
                         <AiFillPlayCircle />
                     </div>
                     <img src={props.imagem} alt="" />
 
                 </div>
             </Link>
+        
         </div>
-
+    
     )
 }
 export default EpisodiosHome
--- src/components/layoult/VideoProgress/VideoProgress.js
+++ src/components/layoult/VideoProgress/VideoProgress.js
@@ -3,8 +3,8 @@ import styles from './VideoProgress.module.css'
 function VideoProgress(props) {
     return (
         <div className={styles.BarraTotal}>
-             
-            <dir className={styles.BarraConcluido} style={{width: `${props.porcentagem}%`}}>.</dir>
+            
+            <dir className={styles.BarraConcluido} style={{width: `${props.porcentagem}%`}}></dir>
         </div>
     )
 }
