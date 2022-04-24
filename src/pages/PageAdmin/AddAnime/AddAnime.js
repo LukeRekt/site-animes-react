a3b97376d26b367239480e355aec936894f84cc2
LukeRekt <lucasrsl12@gmail.com>
2022-04-24 18:31:07 -0300

    editar noticias admin

--- src/pages/PageAdmin/AddAnime/AddAnime.js
+++ src/pages/PageAdmin/AddAnime/AddAnime.js
@@ -1,3 +1,5 @@
+import styles from './AddAnime.module.css'
+
 function AddAnime(){
 
     return (
---
+++ src/pages/PageAdmin/EditarNoticias/EditarNoticias.js
@@ -0,0 +1,7 @@
+import styles from './EditarNoticias.module.css'
+
+function EditarNoticias() {
+    return ( <>Editar Noticias</>)
+}
+
+export default EditarNoticias
