453336e1029bc831512982483f2df1fa88c05c17
LukeRekt <lucasrsl12@gmail.com>
2022-04-24 18:19:24 -0300

    episodios listados na aba de admin

---
+++ src/pages/PageAdmin/ListAnimesAdmin/ListAnimesAdmin.js
@@ -0,0 +1,47 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import styles from "./ListAnimesAdmin.module.css"
+import { HiEmojiSad } from "react-icons/hi";
+function ListAnimesAdmin (props){
+    const [AnimesAdmin, setAnimesAdmin] = useState()
+    const [CarregandoAnimesAdmin, setCarregandoFavoritos] = useState(true)
+
+
+    useEffect(() => {
+        axios.get('http://localhost:3232/getanim')
+            .then(res => {
+                setAnimesAdmin(res.data.animes)
+                setCarregandoFavoritos(false)
+                console.log(AnimesAdmin.length)
+            })
+    }, [])
+
+    return (
+        
+       <div className={styles.container}>
+  
+  {CarregandoAnimesAdmin ? (<>Carregando</>) : ( AnimesAdmin.length == 0 ? (<div id={styles.nenhumFav}><p> Você não tem nenhum favorito <HiEmojiSad/></p></div>) : (AnimesAdmin.map((teste, index) => {
+                    return <div key={index} className={styles.animeCapa}>
+                    <Link to={`/anime/${teste.id}`}>
+                    <div className={styles.animeOver}>
+                    <small>
+                    <p>{teste.nome} </p>
+                    </small>
+                    </div>
+                    <img src={teste.imagem} alt="" />
+                    </Link>
+                    </div>
+                
+                {/* {console.log(teste.idAnime, teste.nomeAnime, teste.nomeUsuario, teste.imagemAnime)} */}
+                 
+               
+               
+        })))} 
+       </div>
+        
+        
+    )
+}
+
+export default ListAnimesAdmin
--- src/pages/PageAdmin/PageAdmin.js
+++ src/pages/PageAdmin/PageAdmin.js
@@ -4,13 +4,13 @@ import { UserContext } from '../../UserContext'
 import { FaHeart, FaClock, FaEye} from 'react-icons/fa';
 import { RiEqualizerLine } from "react-icons/ri";
 import { useState, useEffect } from "react";
+import ListAnimesAdmin from "./ListAnimesAdmin/ListAnimesAdmin";
 function PageAdmin () { 
     const { user, setUser } = useContext(UserContext);
 
     const [toggleState, setToggleState] = useState(5);
 
     const toggleTab = (index) => {
-        setPageCarregando(false);
         setToggleState(index);
     };
         
@@ -22,14 +22,14 @@ function PageAdmin () {
                 <div onClick={() => toggleTab(1)} className={toggleState === 1 ? `${styles.favoritos} ${styles.ativo} ${styles.tab_color}`: `${styles.favoritos} ${styles.tab_color}`} ><p><FaHeart /> Usuarios</p></div>
                 <div onClick={() => toggleTab(2)} className={toggleState === 2 ? `${styles.assistidos} ${styles.ativo} ${styles.tab_color}`: `${styles.assistidos} ${styles.tab_color}`}><p><FaEye /> Animes</p></div>
                 <div onClick={() => toggleTab(3)} className={toggleState === 3 ? `${styles.maistarde} ${styles.ativo} ${styles.tab_color}`: `${styles.maistarde} ${styles.tab_color}`}><p><FaClock /> Noticias</p></div>
-                <div onClick={() => toggleTab(5)} className={toggleState === 5 ? `${styles.editarPerfil} ${styles.ativo} ${styles.tab_color}` : `${styles.editarPerfil} ${styles.tab_color}`}><p><RiEqualizerLine /> P</p></div>
+                <div onClick={() => toggleTab(4)} className={toggleState === 4 ? `${styles.editarPerfil} ${styles.ativo} ${styles.tab_color}` : `${styles.editarPerfil} ${styles.tab_color}`}><p><RiEqualizerLine /> P</p></div>
                 
 
             </div>
             <div className={styles.content_tabs}>
                 <div className={toggleState === 1 ? `${styles.content}  ${styles.active_content}` : `${styles.content}`}>
                     <div className={styles.tab_Favoritos}>
-                  
+                  <ListAnimesAdmin/>
                         
                     </div>
 
@@ -47,16 +47,13 @@ function PageAdmin () {
                     </div>
                 </div>
                 <div className={toggleState === 4 ? `${styles.content}  ${styles.active_content}` : `${styles.content}`}>
-                    <div className={styles.naoTem}>
+                <div className={styles.naoTem}>
                         
-                        
-                    </div>
+                        </div>
                 </div>
-                <div className={toggleState === 5 ? `${styles.content}  ${styles.active_content}` : `${styles.content}`}>
-                     
                 </div>
+               
             </div>
-        </div>
     )
 }
 
