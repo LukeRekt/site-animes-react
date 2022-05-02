2610c0af0e46a65b1a9edd78e4d05ebc433f28ac
LukeRekt <lucasrsl12@gmail.com>
2022-05-02 20:37:56 -0300

    dados adicionados ao formulario

--- src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarAnime.js
+++ src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarAnime.js
@@ -6,12 +6,39 @@ import styles from './EditarAnime.module.css'
 function EditarAnime(){
     const { id } = useParams();
     const [posts, setPosts] = useState([])
+
+    //valores do form
+    const [nome, setNome] = useState([])
+    const [descricao, setDescricao] = useState([])
+    const [nota, setNota] = useState([])
+    const [episodios, setEpisodios] = useState([])
+    const [diaLancameto, setDiaLancameto] = useState([])
+    const [especiais, setEspeciais] = useState([])
+    const [filmes, setFilmes] = useState([])
+    const [autor, setAutor] = useState([])
+    const [diretor, setDiretor] = useState([])
+    const [estudio, setEstudio] = useState([])
+    const [ano, setAno] = useState([])
+    const [temporadas, setTemporadas] = useState([])
     const [temas, setTemas] = useState([])
 
     useEffect(() => {
         axios.get(`http://localhost:3232/getanim/${id}`)
             .then(res => {
-                setPosts(res.data.animes)
+                // setPosts(res.data.animes)
+                
+                setNome(res.data.animes.nome)
+                setDescricao(res.data.animes.descricao)
+                setNota(res.data.animes.nota)
+                setEpisodios(res.data.animes.episodios)
+                setDiaLancameto(res.data.animes.diaLancameto)
+                setEspeciais(res.data.animes.especiais)
+                setFilmes(res.data.animes.filmes)
+                setAutor(res.data.animes.autor)
+                setDiretor(res.data.animes.diretor)
+                setEstudio(res.data.animes.estudio)
+                setAno(res.data.animes.ano)
+                setTemporadas(res.data.animes.temporadas)
                 setTemas(res.data.animes.temas)
             }).catch((err) => {
                 
@@ -22,9 +49,29 @@ function EditarAnime(){
     return (
         <div className={styles.container}>
             <div className={styles.imagem}>
-            <img src={posts.imagem} alt="" />
+                <img src={posts.imagem} alt="" />
             </div>
-            {console.log(posts)}
+            <div className={styles.formulario}>
+                <form>
+                    <input type="text" value={nome} placeholder='Nome' />
+                    <textarea cols="52" rows="9" placeholder="Descricao" value={descricao}></textarea>
+                    <input type="text" value={nota} placeholder='Nota' />
+                    <input type="text" value={episodios} placeholder='Episodios' />
+                    <input type="checkbox" name="" id="" />
+                    <input type="text" value={diaLancameto} placeholder='Dia de Lancamento' />
+                    <input type="text" value={especiais} placeholder='Especiais' />
+                    <input type="text" value={filmes} placeholder='Filmes' />
+                    <input type="text" value={autor} placeholder='Autor' />
+                    <input type="text" value={diretor} placeholder='Diretor' />
+                    <input type="text" value={estudio} placeholder='Estudio' />
+                    <input type="text" value={ano} placeholder='Ano' />
+                    <input type="file" name="" id="" />
+                    <input type="text" value={temporadas} placeholder='Temporadas' />
+                    <input type="text" value={temas} placeholder='Temas' />
+                    <button>Enviar</button>
+                </form>
+            </div>
+            {console.log(nome)}
             {console.log(temas)}
             </div>
     )
