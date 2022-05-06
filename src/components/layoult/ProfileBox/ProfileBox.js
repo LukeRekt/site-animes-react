253d7d11ad2e55d4bee4505eda3f451390d34b25
LukeRekt <lucasrsl12@gmail.com>
2022-05-06 20:49:15 -0300

    alteracao profilebox

--- src/components/layoult/ProfileBox/ProfileBox.js
+++ src/components/layoult/ProfileBox/ProfileBox.js
@@ -1,6 +1,7 @@
 import styles from './ProfileBox.module.css'
 import { Link, useNavigate } from 'react-router-dom';
-import { BiKey } from "react-icons/bi";
+import { BiKey} from "react-icons/bi";
+import {BsPencilSquare} from "react-icons/bs";
 import { logout } from '../../../api/user';
 import { UserContext } from '../../../UserContext';
 import { toast } from 'react-toastify';
@@ -33,10 +34,15 @@ function ProfileBox(props) {
 
             {props.logado ? (<></>) : (<div className={styles.nomeUser}><p>@{user}</p> </div>)}
             <div className={styles.opcoes}>
+            {props.logado == false ? (<></>) : (
+            
+            <div className={styles.login}><p id={styles.upperTitulo}>Acesse a sua conta ou crie uma e aproveite dos benefícios!</p></div>
+            )}
+
                 {props.logado ? (<div onClick={() => setLoginVisibility(true)} className={styles.login}> <BiKey size="40px" /> <p>Login</p></div>) :
                     (<Link to="/perfil"><div className={styles.login}>  <BiKey size="40px" /><p>Perfil</p></div></Link>)}
 
-                {props.logado ? (<Link to="/registrar"><div className={styles.login}> <BiKey size="40px" /> <p>Registrar</p></div></Link>) :
+                {props.logado ? (<Link to="/registrar"><div className={styles.login}> <BsPencilSquare size="34px" /> <p>Registrar</p></div></Link>) :
                     (<div onClick={handleLogout} className={styles.login}> <AiOutlineLogout size="40px" /> <p>Deslogar</p></div>)}
 
                 {props.logado == false ? (admin == false ? (<></>) : (<Link to="/admin"><div id={styles.admin} className={styles.login}> <FaStar size="40px" /> <p id={styles.admin}>Admin</p></div></Link>)) : (<></>)}
