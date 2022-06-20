3f33e03314ff0b878220517891f3f2982ba7a606
LukeRekt <lucasrsl12@gmail.com>
2022-06-20 11:00:35 -0300

    estilo editar ep

--- src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarEp/EditarEpPage.js
+++ src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarEp/EditarEpPage.js
@@ -3,11 +3,13 @@ import styles from './EditarEpPage.module.css'
 function EditarEpPage(){
     return (
         <div className={styles.container}>
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
+            <form>
+            <input type="text" placeholder='Nome'/>
+            <input type="text" placeholder='Titulo EP'/>
+            <input type="text" placeholder='Link Legendado'/>
+            <input type="text" placeholder='Link Dublado'/>
             <button>Enviar</button>
+            </form>
         </div>
 
     )
