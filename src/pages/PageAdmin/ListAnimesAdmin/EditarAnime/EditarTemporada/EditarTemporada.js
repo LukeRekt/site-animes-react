3e6b403ce566ee38a483d0a833b012dec3384034
LukeRekt <lucasrsl12@gmail.com>
2022-06-28 11:03:19 -0300

    botao adicionar ep

--- src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarTemporada/EditarTemporada.js
+++ src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarTemporada/EditarTemporada.js
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import EditarEpLayout from '../layoult/EditarEp/EditarEpLayoult';
 import styles from './EditarTemporada.module.css'
-
+import { MdOutlineAdd } from "react-icons/md";
 function EditarTemporada(){
     return (
         <div className={styles.container}>
             <EditarEpLayout/>
+            <div className={styles.adicionarEp}>
+            <MdOutlineAdd/>
+            </div>
         </div>
     )
                 
