f92049b5400bc5d89cf2de23630c6cb445570b58
LukeRekt <lucasrsl12@gmail.com>
2022-06-28 09:49:29 -0300

    todas as variáveis da edicao de anime

--- src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarEp/EditarEpPage.js
+++ src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarEp/EditarEpPage.js
@@ -14,27 +14,72 @@ function EditarEpPage(){
 
     const [posts, setPosts] = useState([]);
     const {temporada, id, ep } = useParams();
+
+
+    const [nome, setNome] = useState()
+    const [video, setVideo] = useState()
+    const [videoDublado, setVideoDublado] = useState()
+    const [animeImagem, setAnimeImagem] = useState()
+    const [inicioAbertura, setInicioAbertura] = useState()
+    const [fimAbertura, setFimAbertura] = useState()
+ 
+
     useEffect(() => {
       
         axios.get(`http://localhost:3232/getanim/episodios/${id}/${temporada}/${ep}`)
           .then(res => {
-            setPosts(res.data.episodios)
-            // console.log(posts.nome)
+            setNome(res.data.episodios.nome)
+            setVideo(res.data.episodios.video)
+            setVideoDublado(res.data.episodios.videoDublado)
+            setAnimeImagem(res.data.episodios.animeImagem)
+            setInicioAbertura(res.data.episodios.inicioAbertura)
+            setFimAbertura(res.data.episodios.fimAbertura)
+            
           }).catch((err) => {
             console.log(err)
           })
       }, [])
+      const variables = {
+        id: id,
+        nome: nome,
+        video: video,
+        videoDublado: videoDublado,
+        thumbnail: animeImagem,
+        inicioAbertura: inicioAbertura,
+        fimAbertura: fimAbertura,
+
+
+        
+    }
+    const formData = new FormData();
+  
+
+    formData.append('data', JSON.stringify(variables));
+    const handleRegister = async (e) => {
+            axios.post('http://localhost:3232/Testes', formData, { withCredentials: true }, {
+                headers: {
+                //   'Authorization': `${cookies.get('jwt')}`,
+                'Content-Type': 'application/json'
+                }
+              })
+              .catch((error) => {
+                console.error(error)
+              })
+        
+}
+
     return (
         <div className={styles.container}>
             <form>
-            <input type="text" placeholder='Titulo EP' value={posts.nome}/>
-            <input type="text" placeholder='Link Legendado' value={posts.video}/>
-            <input type="text" placeholder='Link Dublado' value={posts.videoDublado}/>
-            <input type="text" placeholder='Thumbnail' value={posts.animeImagem}/>
-            <input type="text" placeholder='Inicio Abertura' value={posts.inicioAbertura}/>
-            <input type="text" placeholder='Fim Abertura' value={posts.fimAbertura}/>
+            <input type="text" placeholder='Titulo EP' value={nome} onChange={(e) => setNome(e.target.value)}/>
+            <input type="text" placeholder='Link Legendado' value={video} onChange={(e) => setVideo(e.target.value)}/>
+            <input type="text" placeholder='Link Dublado' value={videoDublado} onChange={(e) => setVideoDublado(e.target.value)}/>
+            <input type="text" placeholder='Thumbnail' value={animeImagem} onChange={(e) => setAnimeImagem(e.target.value)}/>
+            <input type="text" placeholder='Inicio Abertura' value={inicioAbertura} onChange={(e) => setInicioAbertura(e.target.value)}/>
+            <input type="text" placeholder='Fim Abertura' value={fimAbertura} onChange={(e) => setFimAbertura(e.target.value)}/>
             <button>Enviar</button>
             </form>
+            
         </div>
 
     )
