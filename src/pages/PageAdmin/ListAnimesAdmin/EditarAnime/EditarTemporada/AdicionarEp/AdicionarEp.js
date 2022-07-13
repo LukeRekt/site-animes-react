c094a91d1fbd38e25d643ae9fd54977bac0a0fa4
LukeRekt <lucasrsl12@gmail.com>
2022-07-13 12:19:34 -0300

    inputs editados

--- src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarTemporada/AdicionarEp/AdicionarEp.js
+++ src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarTemporada/AdicionarEp/AdicionarEp.js
@@ -5,14 +5,13 @@ function AdicionarEp(){
     return (
         <div>
             <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-
+        <input type="text"  placeholder='Nome'/>
+        <input type="text" placeholder='Video'/>
+        <input type="text" placeholder='Anime Imagem'/>
+        <input type="text" placeholder='Video Dublado'/>
+        <input type="text" placeholder='Inicio Abertura'/>
+        <input type="text" placeholder='Fim Abertura'/>
+        <button>Enviar</button>
             </form>
 
         </div>
