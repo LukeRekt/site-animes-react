122dde48e698da1b257bd09baac4221deb1741de
LukeRekt <lucasrsl12@gmail.com>
2022-07-18 10:33:17 -0300

    episódios buscados a partir da api

--- src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarTemporada/AdicionarEp/AdicionarEp.js
+++ src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarTemporada/AdicionarEp/AdicionarEp.js
@@ -1,9 +1,24 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import styles from './AdicionarEp.module.css'
 
 function AdicionarEp(){
+    const { id, temporada } = useParams();
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+
+        
+        axios.get(`http://localhost:3232/getanim/episodios/${temporada}/${id}/`)
+            .then(res => {
+                setPosts(res.data.episodios)
+                // console.log(posts)
+            })
+    }, [])
     return (
         <div className={styles.container}>
+            {console.log(posts.length)}
             <form>
         <input type="text"  placeholder='Nome'/>
         <input type="text" placeholder='Video'/>
--- src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarTemporada/EditarTemporada.js
+++ src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/EditarTemporada/EditarTemporada.js
@@ -2,13 +2,15 @@ import { useState } from 'react';
 import EditarEpLayout from '../layoult/EditarEp/EditarEpLayoult';
 import styles from './EditarTemporada.module.css'
 import { MdOutlineAdd } from "react-icons/md";
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 function EditarTemporada(){
+    const { id, temporada } = useParams();
+    
     return (
         <div className={styles.container}>
             <EditarEpLayout/>
             <div className={styles.adicionarEp}>
-            <Link to={`/admin/adicionar/1/1`}>
+            <Link to={`/admin/adicionar/${id}/${temporada}`}>
             <MdOutlineAdd/>
             </Link>
             </div>
