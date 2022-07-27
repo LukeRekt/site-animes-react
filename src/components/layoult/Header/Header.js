dee8ce84ae18c9f5b8d8c14d2ad75b5d85a83c8e
LukeRekt <lucasrsl12@gmail.com>
2022-07-27 11:53:44 -0300

    corrigido avatar usuário

--- src/components/layoult/Header/Header.js
+++ src/components/layoult/Header/Header.js
@@ -41,7 +41,7 @@ function Header() {
             <div className={styles.usuario}>
 
                 <div className={styles.perfil}>
-                    {user ? (<img onClick={toggleProfile} className={styles.avatar} src={avatar} alt="Perfil" />) : (<img onClick={toggleProfile} className={styles.avatar} src={`${process.env.REACT_APP_API_URL}/static/imagens/avatars/default.png`} alt="" />)}
+                    {user ? (<img onClick={toggleProfile} className={styles.avatar} src={`${process.env.REACT_APP_API_URL}/${avatar}`} alt="Perfil" />) : (<img onClick={toggleProfile} className={styles.avatar} src={`${process.env.REACT_APP_API_URL}/static/imagens/avatars/default.png`} alt="" />)}
                     <div className={styles.perfilBox}>
 
                         {user ? (menu ? (<ProfileBox logado={false} visibility="visible" opacity={1} />) : (<ProfileBox logado={false} visibility="hidden" opacity={0} />)) : (menu ? (<ProfileBox logado={true} visibility="visible" opacity={1} />) : (<ProfileBox logado={true} visibility="visible" opacity={0} />))}
