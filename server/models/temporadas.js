b7b84638b343472230eaf2a2fe8b46999a26f0e3
LukeRekt <lucasrsl12@gmail.com>
2022-08-29 11:54:25 -0300

    novo model de temporada

---
+++ server/models/temporadas.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const temporadaSchema = new mongoose.Schema(
+	{
+		idAnime: {
+			type: Number,
+			required: true,
+			trim: true,
+		},
+		temporadaAnime: {
+			type: Number,
+			required: true,
+			trim: true,
+		},
+		nomeTemporada: {
+			type: String,
+			required:true,
+			trim: true,
+		},
+    }
+);
+
+
+module.exports = mongoose.model("Temporada", temporadaSchema);
