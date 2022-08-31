213aca6cbef1b3f9a6296371b38a6fddaccbddd7
LukeRekt <lucasrsl12@gmail.com>
2022-08-31 09:58:12 -0300

    botao editar temporada

--- src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/layoult/TemporadasEditar.js
+++ src/pages/PageAdmin/ListAnimesAdmin/EditarAnime/layoult/TemporadasEditar.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./TemporadasEditar.module.css"
 import { MdOutlineAdd } from "react-icons/md";
+import { BsFillPencilFill } from "react-icons/bs";
 function TemporadasEditar(props){
 
     return ( <div className={styles.container}>
@@ -10,6 +11,8 @@ function TemporadasEditar(props){
             return <Link to={`/admin/editar/${props.id}/${index + 1}`}>
             <div className={styles.barraTemporada}>
             <p>Temporada: {post.temporadaAnime} - {post.nomeTemporada}</p>
+            <div className={styles.editarTemporada}><BsFillPencilFill/></div>
+            
             </div>
             </Link>
         })}
